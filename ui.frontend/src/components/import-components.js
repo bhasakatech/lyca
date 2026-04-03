fe3a0ac5c6033bd3fc9ed5ebf3358764e34c1cd2
Supriyajaiswal28 <jsupriya.2806@gmail.com>
2026-04-03 11:48:40 +0530

    I have complete Dev-7 Navigation component

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -58,12 +58,11 @@ import JoinLyca from "./JoinLyca/JoinLyca";
 
 import HeroCarousel from "./HeroCarousel/HeroCarousel";
 
-
-
-
 import HeroCarousel from "./HeroCarousel/HeroCarousel";
 
 import BestSimPlans from "./BestSimPlans/BestSimPlans";
+
+import Navigation from "./SpaNavigation/Navigation";
 
 //lazyload / code splitting example of an internal component
 const LazyTextComponent = withAsyncImport(() => import(`./Text/Text`));
@@ -72,6 +71,7 @@ const LazyTextComponent = withAsyncImport(() => import(`./Text/Text`));
 const TitleV2 = withAsyncImport(() => import(`@adobe/aem-core-components-react-base/dist/authoring/title/v2/TitleV2`));
 const CarouselV1 = withAsyncImport(() => import(`@adobe/aem-core-components-react-spa/dist/container/carousel/v1/CarouselV1`));
 
+MapTo("lyca-spa-react/components/content/navigation")(Navigation);
 MapTo('lyca-spa-react/components/lycafeaturecomponent')(LycaFeatureComponent);
 //custom component example by Bhaskar
 MapTo("lyca-spa-react/components/joinLyca")(JoinLyca);
