0cd07bba43856f67d5d70f0775d1e7568b91777b
Saisri-karlapalepu <saisrikarlapalepu100@gmail.com>
2026-04-08 16:16:35 +0530

    I have made changes in LycaFeatureComponent

--- core/src/main/java/com/adobe/aem/lyca/core/models/LycaFeatureComponentImpl.java
+++ core/src/main/java/com/adobe/aem/lyca/core/models/LycaFeatureComponentImpl.java
@@ -26,15 +26,15 @@ public class LycaFeatureComponentImpl implements LycaFeatureComponent {
     public static final String RESOURCE_TYPE="lyca-spa-react/components/lycafeaturecomponent";
 
     @ValueMapValue
-    private String heading;
+    private String featureMainHeading;
 
-    @ChildResource(name = "features")
+    @ChildResource(name = "lycaFeatureCards")
     private List<FeatureItem> items;
 
 
     @Override
     public String getHeading() {
-        return heading;
+        return featureMainHeading;
     }
 
     @Override
