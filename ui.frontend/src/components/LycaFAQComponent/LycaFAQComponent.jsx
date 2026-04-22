f804daedc9155fa6d975a349ae4915d4d022a2c9
Saisri-karlapalepu <saisrikarlapalepu100@gmail.com>
2026-04-22 16:44:32 +0530

    LycaFAQComponent Reduced gap between answer and border line

--- ui.frontend/src/components/LycaFAQComponent/LycaFAQComponent.jsx
+++ ui.frontend/src/components/LycaFAQComponent/LycaFAQComponent.jsx
@@ -7,6 +7,14 @@ const LycaFAQComponent=(props)=>{
   const toggleFaq = (index) => {
     setActiveIndex(index === activeIndex ? -1 : index);
   };
+      const cleanHtml = (html) => {
+    if (!html) return '';
+
+    return html
+      .replace(/<p>(&nbsp;|\s|<br\s*\/?>)*<\/p>/g, '')
+      .replace(/>\s+</g, '><')
+      .trim();
+  };
     return(
         <div className="lyca-faq-container">
             <h2>{props.faqMainHeading}</h2>
@@ -27,7 +35,7 @@ const LycaFAQComponent=(props)=>{
             {activeIndex === index && (
               <div
                 className="faq-answer"
-                dangerouslySetInnerHTML={{ __html: item.ans }}
+                dangerouslySetInnerHTML={{ __html: cleanHtml(item.ans) }}
               />
             )}
 
